3ff62a03d8a2535748bb9e628d6958e225a5a710
Edgar Diaz <ed35376@gmail.com>
2023-05-03 22:33:50 -0400

    secondcounter created

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React , { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
-export default Home;
+	function SecondCounter() {
+		const [count, setCount] = useState(0);
+	
+		useEffect(() => {
+			const interval = setInterval(() => {
+				setCount(count + 1);
+			}, 1000);
+	
+			return () => clearInterval(interval);
+		}, [count]);
+	
+		return (
+			<div className="container bg-black text-white p-4 d-flex ">
+			   <h1 className="my-auto py-5 text-center bg-dark border border-light-subtle border-top-0 border-bottom-0 rounded   w-25" style={{ fontSize: 50 }}><i className="fa-regular fa-clock"></i></h1>
+			   
+				<h1 className=" my-auto py-5 text-center bg-dark border border-light-subtle border-top-0 border-bottom-0 rounded   w-25 " style={{ fontSize: 50 }} >{Math.floor(count / 10000) % 10 }</h1>
+				<h1 className=" my-auto py-5 text-center bg-dark border border-light-subtle border-top-0 border-bottom-0 rounde w-25 " style={{ fontSize: 50 }}>{Math.floor(count / 10000) % 10 }</h1>
+				<h1 className=" my-auto py-5 text-center bg-dark border border-light-subtle border-top-0 border-bottom-0 rounded w-25 " style={{ fontSize: 50 }}>{Math.floor(count / 1000) % 10 }</h1>
+				<h1 className=" my-auto py-5 text-center bg-dark border border-light-subtle border-top-0 border-bottom-0 rounded w-25 " style={{ fontSize: 50 }}>{Math.floor(count / 100) % 10 }</h1>
+				<h1 className="my-auto py-5 text-center bg-dark border border-light-subtle border-top-0 border-bottom-0 rounded w-25 " style={{ fontSize: 50 }}>{Math.floor(count / 10) % 10 }</h1>
+				<h1 className=" my-auto py-5 text-center bg-dark border border-light-subtle border-top-0 border-bottom-0 rounded w-25 " style={{ fontSize: 50 }}>{Math.floor(count / 1) % 10 }</h1>
+			</div>
+		);
+	}
+export default SecondCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,36 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import SecondCounter from "./component/home.jsx"
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+// //import your own components
+// import Home from "./component/home.jsx";
+
+// function CounterSimple(props) {
+//     return (
+//     <>
+//     <div className="Cuerpo"></div>
+// </>
+//     )}
+
+// function SimpleCounter() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <>
+//     <div>
+//       <h1>{count.toString().padStart(6, '0')}</h1>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
+//     </div>
+//     </>
+//   );
+// }
+
+
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SecondCounter />, document.querySelector("#app"));
